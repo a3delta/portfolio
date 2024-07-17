01c258f93626e4444b355bc044d640bdebc6c926
a3delta <alex.b.dillinger@gmail.com>
2024-07-16 19:41:28 -0500

    Create lab1.vhd
    Initial commit of VHDL lab 1.

---
+++ education/fpga/lab1/lab1.vhd
@@ -0,0 +1,27 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity LB1 is
+    Port ( a : in STD_LOGIC;
+           b : in STD_LOGIC;
+           c : in STD_LOGIC;
+           d : in STD_LOGIC;
+           f : out STD_LOGIC);
+end LB1;
+
+architecture Behavioral of LB1 is
+
+begin
+
+    f <= c OR (a AND NOT b AND NOT d);
+
+end Behavioral;
